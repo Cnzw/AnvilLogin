0d08304a532134b7f095d3d968a94d7fbe0a65d9
SamB440 <sam@islandearth.net>
2022-06-09 12:50:49 +0100

    Add 1.19 support

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 
-    compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.19-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.1")
     compileOnly("fr.xephi:authme:5.6.0-SNAPSHOT")
     compileOnly(":FastLoginBukkit")
